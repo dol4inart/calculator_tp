ca575d537c98ea87a51b591fc20119b99e343eb9
Артём Ноздрачёв <a.nozdrachyov@sibstrin.ru>
2024-10-17 00:36:57 +0700

    Добавлена реализация калькулятора (паттерн MVVM)

---
+++ Model/CalculatorModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator.Model
+{
+    public class CalculatorModel
+    {
+        public double CurrentValue { get; set; }
+        public double PreviousValue { get; set; }
+        public string Operation { get; set; }
+
+        public void ExecuteOperation()
+        {
+            switch (Operation)
+            {
+                case "+":
+                    CurrentValue = PreviousValue + CurrentValue;
+                    break;
+                case "-":
+                    CurrentValue = PreviousValue - CurrentValue;
+                    break;
+                case "*":
+                    CurrentValue = PreviousValue * CurrentValue;
+                    break;
+                case "/":
+                    if (CurrentValue != 0)
+                        CurrentValue = PreviousValue / CurrentValue;
+                    else
+                        throw new DivideByZeroException("Cannot divide by zero");
+                    break;
+                case "sqrt":
+                    CurrentValue = Math.Sqrt(CurrentValue);
+                    break;
+                case "x^2":
+                    CurrentValue = Math.Pow(CurrentValue, 2);
+                    break;
+                case "1/x":
+                    if (CurrentValue != 0)
+                        CurrentValue = 1 / CurrentValue;
+                    else
+                        throw new DivideByZeroException("Cannot divide by zero");
+                    break;
+            }
+        }
+
+        public void Clear()
+        {
+            CurrentValue = 0;
+            PreviousValue = 0;
+            Operation = string.Empty;
+        }
+    }
+}
---
+++ ViewModel/CalculatorViewModel.cs
@@ -0,0 +1,108 @@
+﻿using calculator.Model;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace calculator.ViewModel
+{
+    public class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private readonly CalculatorModel _calculatorModel;
+        private string _currentInput;
+
+        public string CurrentInput
+        {
+            get => _currentInput;
+            set
+            {
+                if (_currentInput != value)
+                {
+                    _currentInput = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand NumberCommand { get; }
+        public ICommand OperationCommand { get; }
+        public ICommand EqualsCommand { get; }
+        public ICommand ClearCommand { get; }
+        public ICommand ClearEntryCommand { get; }
+        public ICommand BackspaceCommand { get; }
+        public ICommand InvertSignCommand { get; }
+
+        public CalculatorViewModel()
+        {
+            _calculatorModel = new CalculatorModel();
+            _currentInput = "0";
+
+            NumberCommand = new RelayCommand(OnNumberButtonPressed);
+            OperationCommand = new RelayCommand(OnOperationButtonPressed);
+            EqualsCommand = new RelayCommand(OnEqualsButtonPressed);
+            ClearCommand = new RelayCommand(OnClearButtonPressed);
+            ClearEntryCommand = new RelayCommand(OnClearEntryButtonPressed);
+            BackspaceCommand = new RelayCommand(OnBackspaceButtonPressed);
+            InvertSignCommand = new RelayCommand(OnInvertSignButtonPressed);
+        }
+
+        private void OnNumberButtonPressed(object parameter)
+        {
+            var number = parameter.ToString();
+            if (CurrentInput == "0")
+                CurrentInput = number;
+            else
+                CurrentInput += number;
+        }
+
+        private void OnOperationButtonPressed(object parameter)
+        {
+            _calculatorModel.PreviousValue = double.Parse(CurrentInput);
+            _calculatorModel.Operation = parameter.ToString();
+            CurrentInput = "0";
+        }
+
+        private void OnEqualsButtonPressed(object parameter)
+        {
+            _calculatorModel.CurrentValue = double.Parse(CurrentInput);
+            _calculatorModel.ExecuteOperation();
+            CurrentInput = _calculatorModel.CurrentValue.ToString();
+        }
+
+        private void OnClearButtonPressed(object parameter)
+        {
+            _calculatorModel.Clear();
+            CurrentInput = "0";
+        }
+
+        private void OnClearEntryButtonPressed(object parameter)
+        {
+            CurrentInput = "0";
+        }
+
+        private void OnBackspaceButtonPressed(object parameter)
+        {
+            if (CurrentInput.Length > 1)
+                CurrentInput = CurrentInput.Substring(0, CurrentInput.Length - 1);
+            else
+                CurrentInput = "0";
+        }
+
+        private void OnInvertSignButtonPressed(object parameter)
+        {
+            if (double.TryParse(CurrentInput, out double value))
+                CurrentInput = (-value).ToString();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
